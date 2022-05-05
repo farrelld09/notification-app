ba91d18fc12c0b636c795d2de1f16cc02b85dfcf
Dan Farrell <farrelld09@gmail.com>
2022-05-05 09:09:56 -0400

    better

--- src/features/pokemon/Pokemon-fetcher.jsx
+++ src/features/pokemon/Pokemon-fetcher.jsx
@@ -1,49 +1,63 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 export function PokemonFetcher() {
-    // const [havePokemonBeenFetched, setHasBeenFetched] = useState(false);
-    const [pokedex, updatePokedex] = useState(JSON.parse(window.localStorage.getItem('pokedex')) || []);
-    const [pokemonCount, decrementPokemon] = useState(5);
+  const [pokedex, updatePokedex] = useState(
+    JSON.parse(window.localStorage.getItem("pokedex")) || []
+  );
+  const [pokemonCount, decrementPokemon] = useState(5);
 
-    // const fetchPokemon = () => {
-    //     if (!havePokemonBeenFetched) setHasBeenFetched(true);
-    // }
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (pokemonCount > 0) {
+        decrementPokemon((pokemonCount) => pokemonCount - 1);
+        const pokemonIndex = Math.floor(Math.random() * 150) + 1;
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            if (pokemonCount > 0) {   
-                decrementPokemon(pokemonCount => pokemonCount - 1);
-      
-                const pokemonIndex = Math.floor(Math.random() * 150) + 1;
+        return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonIndex}`)
+          .then((response) => response.json())
+          .then((pokemonData) => {
+            const {
+              id,
+              name,
+              sprites: { front_shiny },
+            } = pokemonData;
 
-                return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonIndex}`)
-                    .then(response => response.json())
-                    .then(pokemonData => {
-                        console.log('pokemonData', pokemonData, 'pokedex', pokedex);
-                        updatePokedex((pokedex) => [pokemonData, ...pokedex]);
-                        window.localStorage.setItem('pokedex', JSON.stringify(pokedex));
-                    });
-            }
-        }, 1000);
-        return () => clearInterval(interval);
-      });
+            const pokedexUpdate = {
+              id,
+              name,
+              photo: front_shiny,
+            };
+
+            updatePokedex((pokedex) => [pokedexUpdate, ...pokedex]);
+            window.localStorage.setItem("pokedex", JSON.stringify(pokedex));
+          });
+      }
+    }, 5000);
+    return () => clearInterval(interval);
+  });
 
   return (
     <div>
-        {pokedex.length ?
-            <div className="current-pokemon">
-                <h3 className="pokemon-alert">Look!  A wild {pokedex[0].name} appeared! </h3>
-                <img src={pokedex[0].sprites.front_shiny}/>
-            </div> 
-            : null 
-        }
-        <ul>
-            {pokedex.length ? pokedex.map((pokemon, i) => {
-                return (
-                    <li key={`${pokemon.name}-${i}`}>{pokemon.name}</li>
-                )
-            }) : null}
-        </ul>
+      {pokedex.length ? (
+        <div className="current-pokemon">
+          <p className="pokemon-alert">
+            Look! A wild {pokedex[0].name} appeared!
+          </p>
+          <img className="pokemon-photo" src={pokedex[0].photo} />
+        </div>
+      ) : null}
+      <h3 className="pokemon-alert">Pokedex</h3>
+      <ul className="pokedex">
+        {pokedex.length
+          ? pokedex.map((pokemon, i) => {
+              return (
+                <li className="pokedex-entry" key={`${pokemon.name}-${i}`}>
+                  <p>{pokemon.name}</p>
+                  <img src={pokemon.photo} />
+                </li>
+              );
+            })
+          : null}
+      </ul>
     </div>
   );
 }
